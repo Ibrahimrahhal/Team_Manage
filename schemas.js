3342a780badd094982ea1167c4a96fe256de7841
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2019-02-06 09:25:45 -0800

    added shcemas object

---
+++ schemas.js
@@ -0,0 +1,14 @@
+var team = {
+  _id: String,
+  teamMembers: arrayOfStrings,
+  randCode: String,
+  teamComments: arrayofStrings,
+  teamTasks: arrayOfKeyValuesPairs
+}
+
+var user = {
+  _id: String,
+  UserName: String,
+  Password: String,
+  teamId: String,
+}
