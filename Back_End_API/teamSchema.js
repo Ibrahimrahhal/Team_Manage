8bb9959e2f7051e5cb6b091c2e3e0f4c7a2f276c
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2019-02-15 01:14:14 -0800

    changes to team schema

--- Back_End_API/teamSchema.js
+++ Back_End_API/teamSchema.js
@@ -1,3 +1,6 @@
+var Joi = require('joi');
+const Extension = require('joi-date-extensions');
+const Joi = BaseJoi.extend(Extension);
 var team = {
   teamMembersIds: [],
   randCode: String,
@@ -7,6 +10,17 @@ var team = {
     teamMemberIdFrom: String,
     teamMemberIdTo: String,
     deadLine: Date
+  }]
+}
+var team_joi = {
+  teamMembersIds: Joi.array().items(Joi.string()).required(),
+  randCode: Joi.string().required(),
+  teamComments: Joi.array().items(Joi.string()).required(),
+  teamTasks: [{
+    task: Joi.string().required(),
+    teamMemberIdFrom: Joi.string().required(),
+    teamMemberIdTo: Joi.string().required(),
+    deadLine: Joi.date().format('YYYY-MM-DD');
   }]
 }
 
