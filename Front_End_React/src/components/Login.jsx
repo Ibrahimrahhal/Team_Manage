7c0eb1d629cbc5f46df2d64f8a0f359af82df418
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2019-03-09 22:25:15 -0800

    binded inputs to states in login comp

--- Front_End_React/src/components/Login.jsx
+++ Front_End_React/src/components/Login.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+state ={
+  UserName:"",
+  Password:""
+}
+  change = (e)=>{
+this.state[e.target.id] = e.target.value;
+this.setState(this.state);
 
+  }
   render() {
+
     return (
       <React.Fragment>
       <br/>
@@ -11,12 +20,12 @@ class Login extends Component {
       <h1 className="font-weight-light text-secondary">Login To Mange Your Assigned Tasks</h1>
         <form className="mt-5 w-75 mx-auto d-flex justify-content-center flex-column align-items-center">
     <div className="form-group d-flex justify-content-center flex-column align-items-center w-100">
-      <label htmlFor="exampleInputEmail1">Username</label>
-      <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+      <label htmlFor="UserName">Username</label>
+      <input type="text" className="form-control" id="UserName" aria-describedby="emailHelp" onChange={this.change} value ={this.state.UserName}/>
     </div>
     <div className="form-group d-flex justify-content-center flex-column align-items-center w-100">
-      <label htmlFor="exampleInputPassword1">Password</label>
-      <input type="password" className="form-control" id="exampleInputPassword1" />
+      <label htmlFor="Password">Password</label>
+      <input type="password" className="form-control" id="Password" value = {this.state.Password}  onChange={this.change} />
     </div>
     <button type="submit" className="btn btn-primary">Login</button>
   </form>
